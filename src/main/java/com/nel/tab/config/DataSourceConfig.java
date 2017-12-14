42f0eb2371d52da1c49acbc1a7695c305063cdd5
huxuezi <nelson_xuezi_hu@126.com>
2017-12-15 00:34:00 +0800

    update

--- src/main/java/com/nel/tab/config/DataSourceConfig.java
+++ src/main/java/com/nel/tab/config/DataSourceConfig.java
@@ -1,20 +1,30 @@
 package com.nel.tab.config;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages="com.nel.tab")
+@EnableTransactionManagement(mode = AdviceMode.PROXY)
 public class DataSourceConfig {
 	
 	@Autowired
 	Environment env;
+	
+//	@Autowired
+//	EntityManagerFactory entityManagerFactory;
 	
 	@Bean
 	public DataSource dataSource() {
@@ -26,6 +36,17 @@ public class DataSourceConfig {
 		return dataSource;
 	}
 	
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+//		LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean = 
+//				new LocalContainerEntityManagerFactoryBean();
+//		containerEntityManagerFactoryBean.setDataSource(dataSource);
+//		containerEntityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//		containerEntityManagerFactoryBean.setPackagesToScan("com.nel.tab");
+//		
+//		return containerEntityManagerFactoryBean;
+//		
+//	}
 	
 	
 }
