4cb3e12700c5d626dab4e0d2f0d57facfaec7dce
huxuezi <xuezierhu@gmail.com>
2018-04-22 20:37:55 +0800

    commit

--- src/main/java/com/nel/tab/controller/RootController.java
+++ src/main/java/com/nel/tab/controller/RootController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -121,6 +122,11 @@ public class RootController extends BaseController{
 		 
 	 }
 	 
+	 @RequestMapping(method = RequestMethod.GET)
+	 public String getSomething() {
+		 
+		 return "dfgsd";
+		 
+	 }
 	 
-
 }
